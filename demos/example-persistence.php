c26834f016345c4502afe4f58a01e57626accc11
abbadon1334 <fdanti@gmail.com>
2019-07-04 13:40:01 +0300

    change mysql to sqlite in memory for testing

--- demos/example-persistence.php
+++ demos/example-persistence.php
@@ -2,55 +2,28 @@
 
 include 'bootstrap.php';
 
+use atk4\dsql\Connection;
 use atk4\ui\App;
 use ATK4PHPDebugBar\DebugBar;
 
-class Country extends \atk4\data\Model
+class User extends \atk4\data\Model
 {
-    public $table = 'country';
+    public $table = 'user';
     public function init()
     {
         parent::init();
-        $this->addField('name', ['actual' => 'nicename', 'required' => true, 'type' => 'string']);
-        $this->addField('sys_name', ['actual' => 'name', 'system' => true]);
-        $this->addField('iso', ['caption' => 'ISO', 'required' => true, 'type' => 'string']);
-        $this->addField('iso3', ['caption' => 'ISO3', 'required' => true, 'type' => 'string']);
-        $this->addField('numcode', ['caption' => 'ISO Numeric Code', 'type' => 'number', 'required' => true]);
-        $this->addField('phonecode', ['caption' => 'Phone Prefix', 'type' => 'number', 'required' => true]);
-        $this->addHook('beforeSave', function ($m) {
-            if (!$m['sys_name']) {
-                $m['sys_name'] = strtoupper($m['name']);
-            }
-        });
-    }
-
-    public function validate($intent = null)
-    {
-        $errors = parent::validate($intent);
-        if (strlen($this['iso']) !== 2) {
-            $errors['iso'] = 'Must be exactly 2 characters';
-        }
-        if (strlen($this['iso3']) !== 3) {
-            $errors['iso3'] = 'Must be exactly 3 characters';
-        }
-        // look if name is unique
-        $c = clone $this;
-        $c->unload();
-        $c->tryLoadBy('name', $this['name']);
-        if ($c->loaded() && $c->id != $this->id) {
-            $errors['name'] = 'Country name must be unique';
-        }
-        return $errors;
+        $this->addField('name');
+        $this->addField('email');
     }
 }
 
 // start DbConnection
-$db = new \atk4\data\Persistence\SQL('mysql:dbname=atk4;host=localhost', 'root', 'root');
+$db = \atk4\data\Persistence::connect('sqlite::memory:');
 
-$model_country = new Country($db);
+$model_user = new User($db);
 
 // Migration : if not exists table create it
-(\atk4\schema\Migration::getMigration($model_country))->migrate();
+(\atk4\schema\Migration::getMigration($model_user))->migrate();
 
 
 $app = new App([
@@ -64,15 +37,18 @@ $debugBar->setAssetsResourcesUrl('../');
 //$debugBar->addDefaultCollectors();
 $debugBar->addATK4PersistenceSQLCollector();
 
+$model_user->insert(['name'=>'test 1', 'email'=>'test1@test.it']);
+$model_user->insert(['name'=>'test 2', 'email'=>'test2@test.it']);
+
 $loader = $app->add('Loader');
 
-$loader->set(function ($l) use ($model_country) {
-    $model_country->tryLoadAny();
+$loader->set(function ($l) use ($model_user) {
+    $model_user->tryLoadAny();
 
-    $country_name = $model_country->get('name');
-    $l->app->getDebugBarCollector('messages')->addMessage('new message :'.$country_name);
+    $name = $model_user->get('name');
+    $l->app->getDebugBarCollector('messages')->addMessage('new message :'.$name);
 
-    $l->add(['Text', 'country name :'.$country_name]);
+    $l->add(['Text', 'country name :'.$name]);
 });
 
 /** @var Button $button */
