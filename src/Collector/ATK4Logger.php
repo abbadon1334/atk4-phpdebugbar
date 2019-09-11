fcd4c310e831a86a6d4c9835c46108e732f247bf
abbadon1334 <fdanti@gmail.com>
2019-09-11 06:59:00 +0300

    Fix nested page debug database queries

--- src/Collector/ATK4Logger.php
+++ src/Collector/ATK4Logger.php
@@ -6,17 +6,20 @@ namespace ATK4PHPDebugBar\Collector;
 
 use atk4\ui\App;
 use DebugBar\DataCollector\MessagesCollector;
+use Psr\Log\LoggerInterface;
 
 class ATK4Logger extends MessagesCollector
 {
     protected $app;
 
-    /** @var \Psr\Log\LoggerInterface */
+    /** @var LoggerInterface */
     protected $app_original_logger;
 
-    public function __construct(App $app)
+    public function __construct(App $app, $name = 'app')
     {
-        if ($app->logger instanceof \Psr\Log\LoggerInterface) {
+        parent::__construct($name);
+
+        if ($app->logger instanceof LoggerInterface) {
             $this->app_original_logger = clone $app->logger;
             unset($app->logger);
         }
@@ -31,10 +34,5 @@ class ATK4Logger extends MessagesCollector
         if ($this->app_original_logger) {
             $this->app_original_logger->log($label, $message);
         }
-    }
-
-    public function getName()
-    {
-        return 'ATKAppLog';
     }
 }
--- src/DebugBar.php
+++ src/DebugBar.php
@@ -10,6 +10,7 @@ use atk4\core\FactoryTrait;
 use atk4\core\InitializerTrait;
 use atk4\data\Persistence;
 use atk4\ui\Exception;
+use ATK4PHPDebugBar\Bridge\ATK4Collector;
 use ATK4PHPDebugBar\Collector\ATK4Logger;
 use DebugBar\DataCollector\DataCollectorInterface;
 use DebugBar\DataCollector\ExceptionsCollector;
@@ -22,7 +23,6 @@ use DebugBar\DataCollector\RequestDataCollector;
 use DebugBar\DataCollector\TimeDataCollector;
 use DebugBar\DebugBarException;
 use DebugBar\JavascriptRenderer;
-use PDO;
 
 class DebugBar
 {
@@ -39,9 +39,6 @@ class DebugBar
     /** @var JavascriptRenderer */
     protected $debugBarRenderer;
 
-    /** @var PDOCollector */
-    protected $PDOCollector;
-
     /**
      * @var string
      */
@@ -49,7 +46,10 @@ class DebugBar
     /**
      * @var string
      */
-    protected $assets_resources_path = 'vendor'.DIRECTORY_SEPARATOR.'maximebf'.DIRECTORY_SEPARATOR.'debugbar'.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'DebugBar'.DIRECTORY_SEPARATOR.'Resources'.DIRECTORY_SEPARATOR;
+    protected $assets_resources_path = 'vendor' . DIRECTORY_SEPARATOR . 'maximebf' . DIRECTORY_SEPARATOR . 'debugbar' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'DebugBar' . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR;
+
+    /** @var TimeDataCollector */
+    protected $timeDataCollector;
 
     /**
      * @throws DebugBarException
@@ -59,7 +59,9 @@ class DebugBar
     {
         $this->_init();
 
-        $this->debugBar = new \DebugBar\DebugBar();
+        $this->timeDataCollector = new TimeDataCollector();
+
+        $this->debugBar         = new \DebugBar\DebugBar();
         $this->debugBarRenderer = $this->debugBar->getJavascriptRenderer();
 
         $this->addCollector(new MessagesCollector());
@@ -82,29 +84,43 @@ class DebugBar
      */
     protected function setUpApp(): void
     {
-        $this->app->addHook('beforeRender', function ($j): void {
+        $this->app->addHook(
+            'beforeRender', function ($j): void {
             $this->processAssets();
-            //$this->app->html->template->appendHTML('HEAD', $this->debugBarRenderer->renderHead());
+        }
+        );
+
+        $this->app->addHook(
+            'beforeOutput', function ($j): void {
             $this->app->html->template->appendHTML('Content', $this->debugBarRenderer->render());
-        });
+        }
+        );
 
-        $this->app->addHook('beforeExit', function ($j): void {
-            if (! headers_sent()) {
-                $this->debugBar->sendDataInHeaders();
+        $this->app->addHook(
+            'beforeExit', function ($j): void {
+            if (!headers_sent()) {
+                $this->debugBar->sendDataInHeaders(false);
             }
-        });
+        }
+        );
 
-        $this->app->addMethod('getDebugBar', function ($app) {
+        $this->app->addMethod(
+            'getDebugBar', function ($app) {
             return $this->getDebugBar();
-        });
+        }
+        );
 
-        $this->app->addMethod('getDebugBarCollector', function ($app, string $name) {
+        $this->app->addMethod(
+            'getDebugBarCollector', function ($app, string $name) {
             return $this->getCollector($name);
-        });
+        }
+        );
 
-        $this->app->addMethod('hasDebugBarCollector', function ($app, string $name) {
+        $this->app->addMethod(
+            'hasDebugBarCollector', function ($app, string $name) {
             return $this->hasCollector($name);
-        });
+        }
+        );
     }
 
     protected function processAssets()
@@ -118,11 +134,11 @@ class DebugBar
         [$required_css, $required_js] = $this->debugBarRenderer->getAssets(null, '');
 
         foreach ($required_css as $css) {
-            $this->app->requireCSS($relative_url.$css);
+            $this->app->requireCSS($relative_url . $css);
         }
 
         foreach ($required_js as $js) {
-            $this->app->requireJS($relative_url.$js);
+            $this->app->requireJS($relative_url . $js);
         }
     }
 
@@ -132,6 +148,27 @@ class DebugBar
     public function getDebugBar(): \DebugBar\DebugBar
     {
         return $this->debugBar;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @throws DebugBarException
+     * @return DataCollectorInterface
+     */
+    public function getCollector(string $name): DataCollectorInterface
+    {
+        return $this->debugBar->getCollector($name);
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function hasCollector(string $name): bool
+    {
+        return $this->debugBar->hasCollector($name);
     }
 
     /**
@@ -165,30 +202,12 @@ class DebugBar
     {
         $this->addCollector(new PhpInfoCollector());
         $this->addCollector(new RequestDataCollector());
-        $this->addCollector(new TimeDataCollector());
         $this->addCollector(new MemoryCollector());
         $this->addCollector(new ExceptionsCollector());
-    }
 
-    /**
-     * @param string $name
-     *
-     * @return DataCollectorInterface
-     * @throws DebugBarException
-     */
-    public function getCollector(string $name): DataCollectorInterface
-    {
-        return $this->debugBar->getCollector($name);
-    }
-
-    /**
-     * @param string $name
-     *
-     * @return bool
-     */
-    public function hasCollector(string $name): bool
-    {
-        return $this->debugBar->hasCollector($name);
+        if (!$this->hasCollector($this->timeDataCollector->getName())) {
+            $this->addCollector($this->timeDataCollector);
+        }
     }
 
     /**
@@ -200,38 +219,19 @@ class DebugBar
     }
 
     /**
-     * @param string|null          $prefix
      * @param Persistence\SQL|null $persistence
      *
-     * @throws Exception
      * @throws DebugBarException
      */
-    public function addATK4PersistenceSQLCollector(?Persistence\SQL $persistence = null, string $prefix = null): void
+    public function addATK4PersistenceSQLCollector(?Persistence\SQL $persistence = null): void
     {
-        $persistence = $persistence ?? $this->app->db ?? null;
-        $prefix = $prefix ?? 'db';
+        $persistence = $persistence ?? $this->app->db;
 
-        $this->addCollectorPDO($persistence->connection->connection(), $prefix);
-    }
+        $pdo = new TraceablePDO($persistence->connection->connection());
+        $this->addCollector(new PDOCollector($pdo, $this->timeDataCollector));
 
-    /**
-     * @param string|null $prefix
-     * @param PDO|null    $pdo
-     *
-     * @throws DebugBarException
-     */
-    public function addCollectorPDO(PDO $pdo = null, string $prefix = null): void
-    {
-        $prefix = $prefix ?? 'db';
-
-        $pdoRead = new TraceablePDO($pdo);
-        $pdoWrite = new TraceablePDO($pdo);
-
-        $pdoCollector = new PDOCollector();
-
-        $pdoCollector->addConnection($pdoRead, $prefix.'-read');
-        $pdoCollector->addConnection($pdoWrite, $prefix.'-write');
-
-        $this->addCollector($pdoCollector);
+        if (!$this->hasCollector($this->timeDataCollector->getName())) {
+            $this->addCollector($this->timeDataCollector);
+        }
     }
 }
